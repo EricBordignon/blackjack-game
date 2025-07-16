52e87e48bb4c70cd44044d0092113e5dea4dea1f
Eric Bordignon <ericj.bordignon@gmail.com>
2025-07-16 10:29:48 -0300

    Changes in the display of the elements and building a better backend

--- index.js
+++ index.js
@@ -5,35 +5,147 @@ const app = express();
 const port = 3000;
 
 let deckId;
+let userPoints = 0;
+let computerPoints = 0;
+let gameOver = false;
 
 app.use(express.static("public"));
 
-async function getCard (id, numberOfCards) {
+function win() {
+  if (computerPoints > 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function lose() {
+  if (userPoints > 21) {
+    return true;
+  } else if (computerPoints > userPoints && computerPoints < 22) {
+    return true;
+  } else {
+    return false;
+  };
+};
+
+async function getCard (id, numberOfCards, currentPlayer) {
   const deck = await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=${numberOfCards}`);
   if (id === "new") {
     deckId = deck.data.deck_id;
   };
 
   let drawCards = [];
+
+  let points = 0;
   for (let i = 0; i < numberOfCards; i++) {
     drawCards.push(deck.data.cards[i].image)
+    if (deck.data.cards[i].value === "JACK" || deck.data.cards[i].value === "KING" || deck.data.cards[i].value === "QUEEN" ) {
+      points += 10;
+    } else if (deck.data.cards[i].value === "ACE") {
+      points += 1;
+    } else {
+      points += parseInt(deck.data.cards[i].value);
+    }
+    
   };
 
-  const handOfCards = {
-    cards: drawCards
+  let handOfCards;
+
+  if (currentPlayer === "user") {
+    userPoints += points;
+    handOfCards = {
+      cards: drawCards,
+      points: userPoints,
+      gameOver: false
+    };
+  } else {
+    computerPoints += points;
+    let nextCard = true;
+    let playerWins = false;
+    if (lose()) {
+      nextCard = false;
+    } else if (win()) {
+      nextCard = false;
+      playerWins = true;
+      
+    };
+    
+    handOfCards = {
+      cards: drawCards,
+      points: computerPoints,
+      next: nextCard,
+      gameOver: playerWins
+    }
   };
+  
 
   return handOfCards;
 };
 
 app.get("/draw", async (req, res) => {
-  res.send(await getCard("new", 2));
+  if (!gameOver) {
+    res.send(await getCard("new", 2, "user"));
+  } else {
+     res.status(400).send({
+      error: "Game over",
+      gameOver: true,
+      message: "You can't take more cards because the game is over."
+    });
+  };
 });
 
 
 app.get("/hit-me", async (req, res) => {
-  res.send(await getCard(deckId, 1));
+  if (!gameOver) {
+    let newCards = await getCard(deckId, 1, "user");
+    if (lose()){
+      newCards.gameOver = true;
+      gameOver = true;
+    };
+    res.send(newCards);
+  } else {
+    res.status(400).send({
+      error: "Game over",
+      gameOver: true,
+      message: "You can't take more cards because the game is over."
+    });
+  };
+  
 });
+
+app.get("/play-again", async (req, res) => {
+  computerPoints = 0;
+  userPoints = 0;
+  gameOver = false;
+  const situation = {
+    ok: true
+  };
+  res.send(situation);
+});
+
+
+app.get("/stand", async (req, res) => {
+  let allCards = [];
+  let next = true;
+  let playerWins = false;
+
+  while (next) {
+    const draw = await getCard(deckId, 1, "computer");
+    allCards.push(...draw.cards); // acumula as imagens das cartas
+    next = draw.next;
+    playerWins = draw.gameOver;
+  }
+
+  gameOver = true;
+
+  res.send({
+    cards: allCards,
+    points: computerPoints,
+    gameOver: playerWins
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- public/src/app.js
+++ public/src/app.js
@@ -1,59 +1,109 @@
-document.querySelector("#draw").addEventListener("click", async () => {
+async function getCard(URL, currentPlayer){
   try {
-    const answer = await fetch("/draw");
+    const answer = await fetch(`/${URL}`);
     if (!answer.ok) {
-      throw new Error("Error to draw card");
-      console.log("error");
+      const errorData = await response.json();
+      alert(errorData.message); 
+      return;
     }
 
-    const cardsImg = await answer.json(); // Aqui vem o objeto com os dados da carta
+    const cardsImg = await answer.json(); 
     console.log("Cards received:", cardsImg);
 
     // DOM manipulation here
     cardsImg.cards.forEach(card => {
       const cardTemplate = document.createElement("div");
+      cardTemplate.classList.add("card-container");
+
       console.log(card);
       cardTemplate.innerHTML = `
         <img src="${card}" alt="card" class="card" onerror="console.log('Erro ao carregar imagem')">
       `;
-      document.querySelector(".user-cards").appendChild(cardTemplate); 
+      if (currentPlayer === "user") {
+        document.querySelector(".user-cards").appendChild(cardTemplate); 
+        document.querySelector("#user-points").innerHTML = `Points: ${cardsImg.points}`;
+        
+        if (URL === "draw") {
+          document.querySelector("#draw").classList.add("hidden");
+          document.querySelector("#hit-me").classList.remove("hidden");
+          document.querySelector(".sub-title").innerHTML = "Your turn";
+        };
 
-      document.querySelector("#draw").classList.add("hidden")
-      document.querySelector("#hit-me").classList.remove("hidden")
+        if (cardsImg.gameOver) {
+          console.log("you lose");
+          document.querySelector("#draw").classList.add("hidden");
+          document.querySelector("#hit-me").classList.add("hidden");
+          document.querySelector("#play-again").classList.remove("hidden");
+          document.querySelector(".sub-title").innerHTML = "Your lose";
+        };
+      } else {
+        document.querySelector(".computer-cards").appendChild(cardTemplate); 
+        document.querySelector("#computer-points").innerHTML = `Points: ${cardsImg.points}`
+        document.querySelector("#draw").classList.add("hidden");
+        document.querySelector("#hit-me").classList.add("hidden");
+        document.querySelector("#play-again").classList.add("hidden");
+        document.querySelector("#computer-turn").classList.remove("hidden");
+        document.querySelector(".sub-title").innerHTML = "Computer turn";
+
+        if (cardsImg.gameOver) {
+          document.querySelector("#draw").classList.add("hidden");
+          document.querySelector("#hit-me").classList.add("hidden");
+          document.querySelector("#computer-turn").classList.add("hidden");
+          document.querySelector("#play-again").classList.remove("hidden");
+          document.querySelector(".sub-title").innerHTML = "You win";
+        } else if(!cardsImg.next) {
+          document.querySelector("#draw").classList.add("hidden");
+          document.querySelector("#hit-me").classList.add("hidden");
+          document.querySelector("#computer-turn").classList.add("hidden");
+          document.querySelector("#play-again").classList.remove("hidden");
+          document.querySelector(".sub-title").innerHTML = "Computer wins";
+        }
+      }
       
+      
+
+      
+
     });
-  
-    
 
   } catch (erro) {
     console.error("Erro na requisição:", erro);
   }
-});
+};
 
-document.querySelector("#hit-me").addEventListener("click", async () => {
-  try {
-    const answer = await fetch("/hit-me");
-    if (!answer.ok) {
-      throw new Error("Error to draw card");
-      console.log("error");
+window.addEventListener("DOMContentLoaded", () => {
+  // Todos os seus eventListeners aqui dentro
+
+  document.querySelector("#draw").addEventListener("click", async () => {
+    await getCard("draw", "user");
+  });
+
+  document.querySelector("#hit-me").addEventListener("click", async () => {
+    await getCard("hit-me", "user");
+  });
+
+  document.querySelector("#play-again").addEventListener("click", async () => {
+    try {
+      const answer = await fetch(`/play-again`);
+      if (!answer.ok) {
+        const errorData = await answer.json();
+        alert(errorData.message); 
+        return;
+      }
+
+      document.querySelector("#draw").classList.remove("hidden");
+      document.querySelector("#hit-me").classList.add("hidden");
+      document.querySelector("#play-again").classList.add("hidden");
+      document.querySelector(".sub-title").innerHTML = "Your turn";
+      document.querySelectorAll(".card-container").forEach((card) => card.remove());
+      document.querySelector("#user-points").innerHTML = "Points: 0";
+      document.querySelector("#computer-points").innerHTML = "Points: 0";
+    } catch (erro) {
+      console.error("Erro na requisição:", erro);
     }
+  });
 
-    const cardsImg = await answer.json(); // Aqui vem o objeto com os dados da carta
-    console.log("Cards received:", cardsImg);
-
-    // DOM manipulation here
-    cardsImg.cards.forEach(card => {
-      const cardTemplate = document.createElement("div");
-      console.log(card);
-      cardTemplate.innerHTML = `
-        <img src="${card}" alt="card" class="card" onerror="console.log('Erro ao carregar imagem')">
-      `;
-      document.querySelector(".user-cards").appendChild(cardTemplate); 
-    });
-  
-    
-
-  } catch (erro) {
-    console.error("Erro na requisição:", erro);
-  }
+  document.querySelector("#stand").addEventListener("click", async () => {
+    const computerCards = await getCard("stand", "computer");
+  });
 });
